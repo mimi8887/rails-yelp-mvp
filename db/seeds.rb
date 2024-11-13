8d4da9f56177853e19239ea724cccc32b79c0dfc
Simone Mine <simonminekoza@gmail.com>
2024-11-13 16:52:51 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+Restaurant.create!(name: 'Dishoom', address: '7 Boundary St, London E2 7JE',
+                   phone_number: '08031159322', category: 'italian')
+puts 'Created Dishoom'
+
+Restaurant.create!(name: 'Pizza Hut', address: '7 pizza St',
+                   phone_number: '0120999031', category: 'italian')
+puts 'Created Pizza Hut'
+
+Restaurant.create!(name: 'Sucre et Sel', address: 'Torstraße 132',
+                   phone_number: '035469877726', category: 'french')
+puts 'Created Sucre et Sel'
+
+Restaurant.create!(name: 'Waffle Brothers', address: 'Wafflestreet 60',
+                   phone_number: '01209990555', category: 'belgian')
+puts 'Created Waffle Brothers'
+
+Restaurant.create!(name: 'Noodles Plus', address: 'noodlestreet 60',
+                    phone_number: '01209990555', category: 'chinese')
+puts 'Created Noodle Plus'
+
+puts "Finished! Created #{Restaurant.count} restaurants."
